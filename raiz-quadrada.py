00496f4905332c604105924bf9320cf450b7c681
gui <guiclemente2003@hotmail.com>
2024-02-23 14:22:07 -0300

    a

--- raiz-quadrada.py
+++ raiz-quadrada.py
@@ -1,7 +1,37 @@
 import math
 import matplotlib.pyplot as plt
 
-# Função para calcular a raiz quadrada usando o método de Newton-Raphson
+SQRT_2 = 1.41421356237309584880
+
+def my_sqrt(x):
+    m, k = math.frexp(x)
+    m, k  = m * 2, k - 1
+
+    #Calcula raiz de m = 1 + f
+    f = m - 1
+    sqrt_f = 1 + f/2*(1 - f/(4 + 2*f))
+
+    #calcula raiz de 2**k
+    neg = False
+    if k < 0:
+        k = -k
+        neg = True
+    
+    if k == 0:
+        sqrt_2k = 1
+    elif k == 1:
+        sqrt_2k = SQRT_2
+    elif k & 1 == 0:
+        sqrt_2k = 1 << (k >> 1)
+    elif k & 1 == 1:
+        sqrt_2k = (1 << ((k - 1) >> 1)) * SQRT_2
+    
+
+    if neg:
+        sqrt_2k = 1/sqrt_2k
+
+    return sqrt_f * sqrt_2k
+
 def newton_raphson_sqrt(A, P):
     x_values = []
     y_values = []
@@ -11,30 +41,52 @@ def newton_raphson_sqrt(A, P):
     x1 = Xo
 
     erro = float('inf')
+    iter_count = 0
 
     while erro > P:
         x_values.append(x1)
         y_values.append(abs(math.sqrt(A) - x1))
-
-        # Atualização da estimativa usando a fórmula de Newton-Raphson
         x0 = x1
         x1 = 0.5 * (x0 + A / x0)
         erro = abs(x1 - x0)
+        iter_count += 1
 
-    return x_values, y_values
+    return x_values, y_values, iter_count
 
-# Valor de A (número dentro da raiz quadrada)
-A = 10
-# Precisão desejada
+
+
+def calcular_A(A):
+    pass
+
+def newton_raphson_sqrt2(A, P):
+    x_values = []
+    y_values = []
+
+    # Chute inicial
+    Xo = my_sqrt(A)
+    x1 = Xo
+
+    erro = float('inf')
+    iter_count = 0
+
+    while erro > P:
+        x_values.append(x1)
+        y_values.append(abs(math.sqrt(A) - x1))
+        x0 = x1
+        x1 = 0.5 * (x0 + A / x0)
+        erro = abs(x1 - x0)
+        iter_count += 1
+
+    return x_values, y_values, iter_count
+
+
+A = 16
 P = 1e-5
 
-# Calcula as estimativas de raiz quadrada usando o método de Newton-Raphson
-x_values, y_values = newton_raphson_sqrt(A, P)
-
-# Calcula a raiz quadrada real de A
+x_values, y_values, iter_count_nr = newton_raphson_sqrt(A, P)
 real_sqrt = math.sqrt(A)
+x1_values, y1_values, iter_count_s = newton_raphson_sqrt2(A, P)
 
-# Plotagem do gráfico das diferenças entre a raiz quadrada real e as estimativas de raiz quadrada em cada iteração
 plt.plot(range(len(x_values)), [real_sqrt] * len(x_values), label='Raiz Quadrada Real', linestyle='--')
 plt.plot(range(len(x_values)), x_values, marker='o', label='Estimativa de Newton-Raphson')
 plt.xlabel('Iteração')
@@ -42,4 +94,11 @@ plt.ylabel('Valor')
 plt.title('Comparação entre a Raiz Quadrada Real e as Estimativas de Newton-Raphson')
 plt.legend()
 plt.grid(True)
+plt.show()
+
+
+plt.bar(['Newton-Raphson', 'my_sqrt'], [iter_count_nr, iter_count_s])
+plt.xlabel('Método')
+plt.ylabel('Número de iterações')
+plt.title('Número de iterações para cada método')
 plt.show()
